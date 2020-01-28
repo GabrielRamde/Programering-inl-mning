accd2ac67510c198a8f1306986676757a085c0c8
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2020-01-28 13:34:21 +0100

    Update Program.cs
    kommentering

--- inlämning programering/inlämning programering/Program.cs
+++ inlämning programering/inlämning programering/Program.cs
@@ -8,27 +8,27 @@ namespace BubbleSort
     {
         static void BubbleSort(int[] data)
         {
-            bool sortering = true;
+            bool sortering = true;  //Påståendet nedanför är sant
             
-            for (int i = 0; i < data.Length - 1 && sortering; i++)
+            for (int i = 0; i < data.Length - 1 && sortering; i++) //for loop för alla tal som ska sorteras
             {
                 
-                sortering = false;
+                sortering = false; //Börja om med en till sortering
                 
-                for (int j = 0; j < data.Length - 1 - i; j++)
+                for (int j = 0; j < data.Length - 1 - i; j++) //for loop gå igenom talen igen, (inte talen i variabel "i")
                 {
                     
-                    if (data[j] > data[j + 1])
+                    if (data[j] > data[j + 1])  //Flyttar fram större tal
                     {
                         
-                        sortering = true;
-                      
+                        sortering = true;   //Fortsätta sortera
+                        //byter plats på talen
                         int k = data[j + 1];
                         data[j + 1] = data[j];
                         data[j] = k;
                     }
                 }
-               
+                         //ifall inga tal är sorterar stoppar sortering = false; loopen
             }
         }
         static int[] Data(int m)
@@ -43,8 +43,8 @@ namespace BubbleSort
         {
             int[] n = new int[] { 100, 10000, 100000};
             for (int i = 0; i < n.Length; i++)
-            {
-                Console.WriteLine("Skapar slumpad data av längd " + n[i]);
+            {           
+                Console.WriteLine("Skapar slumpad data av längd " + n[i]);    //timer "DateTime" för att räkna utt hur lång tid det tog att sortera talen
                 int[] data = Data(n[i]);
                 Console.WriteLine("Sorterar slumpad data");
                 DateTime startTid = DateTime.Now;
